4a50b08b90b159992ef76a897451d41f7329b42b
kishor.vasundhara <kishor@vasundhara.com>
2024-09-24 16:59:49 +0530

    (Add) Nodata found page

---
+++ src/components/comman/NoDataAvailable.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NoDataAvailable() {
+    return (
+        <>
+            <p className='no-data-available my-3'>No data available</p>
+        </>
+    )
+}
+
+export default NoDataAvailable
